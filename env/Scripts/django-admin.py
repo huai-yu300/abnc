5d6c76fb16504e7d565c1aea37a8f35966759a89
lyy <18729612053@163.com>
2025-11-18 14:07:24 +0800

    first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Desktop\软件测试项目实战\接口测试实战项目\项目\studentManagementSystem\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
